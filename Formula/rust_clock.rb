7864f1392d9f1d622fd6e989cbd9ba7d732b459c
hoothin <rixixi@gmail.com>
2023-06-09 13:16:51 +0800

    Update rust_clock.rb

--- Formula/rust_clock.rb
+++ Formula/rust_clock.rb
@@ -2,7 +2,7 @@ class RustClock < Formula
   desc "A digital clock for desktop popup every half hour, support 20-20-20 rule"
   homepage "https://github.com/hoothin/RustClock"
   url "https://github.com/hoothin/RustClock/releases/download/0.1.5/RustClock_0.1.5_mac_brew.tar.gz"
-  sha256 "5348cea6a0a63a8141ee5d05b184ba6c5ed1a9d760172681d20b1621801cfebe"
+  sha256 "bc4578c7324f29cf83b2b98cfe65c2b7228789c9e2fa4e01088ee913db39ff80"
   version "0.1.5"
   def install
     libexec.install Dir["*"]
